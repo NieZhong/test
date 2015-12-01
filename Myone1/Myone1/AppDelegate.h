a8e395257198075c754a4ca51910d366f9c14eeb
shirley@gmail.com <tarena22@tiananzhong.local>
2015-12-01 16:18:28 +0800

    aSDFGHJKL;DRFTGYH

---
+++ Myone1/Myone1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myone1
+//
+//  Created by tarena22 on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
